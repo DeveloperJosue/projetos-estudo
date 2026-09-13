bcdc9a3cdf09a6b6673b664ad65b69c04ff76fcf
Josué Santos <developerjosues@gmail.com>
2026-09-13 20:48:44 -0300

    Comentando código

--- Projetos Python/CadastroPessoal.py
+++ Projetos Python/CadastroPessoal.py
@@ -1,38 +1,48 @@
+#definindo as variáveis
 galera = list()
 pessoa = dict()
 soma = media = 0
 
+#importando a biblioteca time para utilizar a função sleep
 import time
 
-print(' Cadastro Pessoal ')
+print(' Cadastro Pessoal ') 
 time.sleep(1)
-while True:
+
+while True: #Definindo o nome da pessoa
     pessoa.clear()
-    pessoa['nome'] = str(input('Nome: '))
-    while True:
+    pessoa['nome'] = str(input('Nome: '))  
+
+    while True: #Validação do sexo
         pessoa['sexo'] = str(input('Sexo [M/F]: ')).upper() [0]
         if pessoa['sexo'] in 'MF':
             break
         print('ERRO! Por favor, digite apenas M ou F.')
-    pessoa['idade'] = int(input('Idade: '))
+
+    pessoa['idade'] = int(input('Idade: '))  #Validação da idade
     soma += pessoa['idade']
     galera.append(pessoa.copy())
-    while True:
+
+    while True: #Validação da resposta para continuar ou não
         resp = str(input('Quer continuar? [S/N] ')).upper() [0]
         if resp in 'SN':
             break
         print('ERRO! Responda apenas S ou N! ')
     if resp == 'N':
         break
+    
+#Resultados
 print('-=' * 30) 
 print(f'Ao todo foram {len(galera)} pessoas cadastradas')
 media = soma / len(galera)
 print(f' A média de idade é de {media:5.2f}. ')
 print(f'As mulheres cadastradas foram', end='')
-for p in galera:
+
+for p in galera: #Iterando sobre a lista de pessoas
     if p['sexo'] == 'F':
         print(f' {p["nome"] }' , end='')
 print()
+#Lista de pessoas que estão acima da média da idade
 print('Lista de pessoas que estão acima da média da idade:')
 for p in galera:
     if p ['idade'] >= media:
@@ -40,4 +50,5 @@ for p in galera:
         for k, v in p.items():
             print(f'{k} = {v}; ', end='')
         print()
+
 print('<< ENCERRADO >>')
